bf07fc96fee5ece3abb1382d4e570f944a009d87
Nitin Chadha <upcomingnewton@gmail.com>
2012-07-02 21:45:12 +0530

    gtk renderer added a callback file

--- gtk/sGtk.h
+++ gtk/sGtk.h
@@ -1,1 +1,4 @@
 #define sGTK_UI_FILE  "xforms/sGtkSample.glade"
+#include <gtk/gtk.h>
+
+gboolean  on_window_destroy  (GtkWidget *widget,GdkEvent  *event, gpointer   user_data);
---
+++ gtk/sGtkCallbacks.c
@@ -0,0 +1,9 @@
+#include <gtk/gtk.h>
+#include "sGtk.h"
+
+/*void */
+/*on_window_destroy (GtkWidget *widget, gpointer user_data)*/
+/*{*/
+/*	fprintf(stdout,"\n destroy event called");*/
+/*    gtk_main_quit ();*/
+/*}*/
